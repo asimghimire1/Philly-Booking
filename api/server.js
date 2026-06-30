179b23ece041eebd438733ed2fb11cd38141f841
asimghimire1 <asimghmr@gmail.com>
2026-06-30 22:10:08 +0545

    same tz as philadelphia

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+process.env.TZ = 'America/New_York'
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const express = require('express');
 const cors = require('cors');
